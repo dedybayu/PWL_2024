2527a50975a069a4ccaa323c2fda18def84014bb
Dedy Bayu Setiawan <dedybayusetiawan5@gmail.com>
2025-02-19 09:28:57 +0700

    parcobaan 1 Route Parameters

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function hello(){
+        return 'Selamat Datang';
+    }
+    public function about(){
+        return 'Dedy Bayu Setiawan (2341720041)';
+    }
+    public function articles($id){
+
+        return "Halaman Artikel dengan Id {$id}";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,33 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use App\Http\Controllers\WelcomeControler;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return 'Selamat Datang';
+Route::get('/user/{name?}', function ($name = 'bayu') {
+    return 'Nama saya ' . $name;
 });
 
-Route::get('/world', function () {
-    return 'World';
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
 });
 
-Route::get('/about', function () {
-    return '2341720041';
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID: ' . $id;
 });
 
-route::get('/hello', [WelcomeControler::class,'hello']);
+
+
+
+
+
+Route::get('/', [PageController::class, 'hello']);
+
+Route::get('/about', [PageController::class, 'about']);
+
+Route::get('/articles', [PageController::class, 'articles']);
+
+route::get('/hello', [WelcomeControler::class, 'hello']);
 
 
 
