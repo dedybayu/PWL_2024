8dd3b1e166b31ded494d9ad01bf87a99da40a635
Dedy Bayu Setiawan <dedybayusetiawan5@gmail.com>
2025-02-19 10:35:18 +0700

    add: views

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -14,6 +14,6 @@ class PageController extends Controller
     }
     public function articles($id){
 
-        return "Halaman Artikel dengan Id {$id}";
+        return "Halaman Artikel dengan Id: {$id}";
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,53 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\PhotoController;
 use App\Http\Controllers\WelcomeControler;
+use Illuminate\Foundation\Console\AboutCommand;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/user/{name?}', function ($name = 'bayu') {
 //     return 'Nama saya ' . $name;
 // });
 
-Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
-    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
-});
+// Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+//     return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+// });
 
-Route::get('/articles/{id}', function ($id) {
-    return 'Halaman Artikel dengan ID: ' . $id;
-});
-
-
-Route::get('/user/{name?}', function ($name = 'John') {
-    return 'Nama saya ' . $name;
-});
+// Route::get('/articles/{id}', function ($id) {
+//     return 'Halaman Artikel dengan ID: ' . $id;
+// });
 
 
-Route::get('/', [PageController::class, 'hello']);
+// Route::get('/user/{name?}', function ($name = 'John') {
+//     return 'Nama saya ' . $name;
+// });
 
-Route::get('/about', [PageController::class, 'about']);
 
-Route::get('/articles', [PageController::class, 'articles']);
+Route::get('/', [HomeController::class, 'hello']);
+
+Route::get('/about', [AboutController::class, 'about']);
+
+Route::get('/articles/{id}', [ArticleController::class, 'articles']);
 
 route::get('/hello', [WelcomeControler::class, 'hello']);
 
+Route::resource('photos', PhotoController::class);
 
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
+
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+]);
+
+Route::get('/greeting', function(){
+    return view('blog.hello', ['name'=>'Bayu']);
+});
 
 
 /*
